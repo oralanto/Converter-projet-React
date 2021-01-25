55dfb8af22c9bcb56d71ba7c619bc10c803bf0a9
Osee-Ralantoarison <o.ralanto@gmail.com>
2021-01-25 20:37:07 +0100

    Composant Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.scss';
+
+const Header = () => (
+  <div className="header">
+    <h1 className="header__title">Converter</h1>
+    <h2 className="header__subtitle">1 euro</h2>
+  </div>
+);
+
+export default Header;
