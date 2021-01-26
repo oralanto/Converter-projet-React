f95c1b6d00ca7378588e075a44f896fec237c7bb
Osee-Ralantoarison <o.ralanto@gmail.com>
2021-01-26 13:58:25 +0100

    Ajout state toggle de currencies

--- src/components/converter/index.js
+++ src/components/converter/index.js
@@ -7,15 +7,31 @@ import Amount from 'src/components/Amount';
 import currenciesData from 'src/data/currencies';
 import './style.scss';
 
-const Converter = () => (
-  <div className="converter">
-    <Header baseAmount={1} />
-    <Currencies currencies={currenciesData} />
-    <Amount
-      value={1.09}
-      currency="United State Dollar"
-    />
-  </div>
-);
+class Converter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    };
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      open: !this.state.open,
+    });
+  }
+
+  render() {
+    return (
+      <div className="converter">
+        <Header baseAmount={1} />
+        <button type="button" onClick={this.toggle}>Toggle</button>
+        {this.state.open && <Currencies currencies={currenciesData} />}
+        <Amount value={1.09} currency="United State Dollar" />
+      </div>
+    );
+  }
+}
 
 export default Converter;
